fb954344c022a5be06c7d2b02354b2c7a64dca48
Henry Velasquez <hvelas93@gmail.com>
2021-02-19 16:56:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,77 @@
+<<<<<<< HEAD
+=======
+# data={
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
+
+#desired data structure
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
+#   "Ms. K" => {
+#     :color => ["grey", "white"],
+#     :gender => ["female"],
+#     :lives => ["Central Park"]
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => ["female"],
+#     :lives => ["Subway"]
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => ["male"],
+#     :lives => ["City Hall"]
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   }
+# }
+
+# .includes?
+# .map { |  }
+# .join
+# .any?
+# .all?
+#.collect?
+# .sort_by
+
+>>>>>>> 4980908a90cb1cf069dbd3a35d65a86684e69eed
 require 'pry'
 
 def nyc_pigeon_organizer(data)
 
+<<<<<<< HEAD
     final_results=data.each_with_object({}) do |(key,value), final_array|
         #binding.pry
         value.each do |inner_key,names|
@@ -19,5 +89,18 @@ def nyc_pigeon_organizer(data)
        end
      end
   #binding.pry
+=======
+        data.each_with_object({}) do |(key,value), final_array|
+        #binding.pry
+        value.each do |inner_key,names|
+          names.each do |name|
+            binding.pry
+        end
+
+        final_array
+
+        end
+   binding.pry
+>>>>>>> 4980908a90cb1cf069dbd3a35d65a86684e69eed
 end
 #thank you soundaraya
